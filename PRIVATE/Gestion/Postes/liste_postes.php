53d45be7303d7be76d8bdfc846d9155a008ceaf1
Gauthier <gauthierlebon97421@gmail.com>
2022-06-15 13:33:13 +0200

    bonne avancée

--- PRIVATE/Gestion/Postes/liste_postes.php
+++ PRIVATE/Gestion/Postes/liste_postes.php
@@ -7,17 +7,19 @@ $db = "SAE24";
 $db = new mysqli("192.168.50.214", $user,$pass,$db)
 or die ("unable to connect");
 
-$sql = "SELECT nom FROM Postes";
+$sql = "SELECT Postes.nom AS metier,AVG(Employés.salaire) AS moyenne FROM Postes INNER JOIN Employés ON Employés.poste = Postes.id_poste GROUP BY metier ;";
+
 $result = mysqli_query($db,$sql) or die ("bad query");
 ?>
 
+
 <center> <h1>Liste des employés </center></h1>
 <center><table border = '1'>
-<tr><td><strong> Nom </strong></td></tr>
+<tr><td><strong> Nom </strong></td><td><strong> Moyenne des salaire par poste </strong></td></tr>
 
 <?php
 while($row=mysqli_fetch_assoc($result)){
-    echo"<tr><td>{$row['nom']}</td>";
+    echo"<tr><td>{$row['metier']}</td><td>{$row['moyenne']}</td>";
     
 }
 ?>
--- PUBLIQUE/index.php
+++ PUBLIQUE/index.php
@@ -45,7 +45,37 @@
   </header>
 
   <main class="px-3">
-  
+  <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Bienvenue sur Toporun.</h2>
+        </br>
+        <p class="lead">TopoRun est un cabinet de <strong>géomètre topographe</strong>, qui propose son expertise dans un ensemble de domaines lié à la topographie. Nous siègons sur l'île de la Réunion et avons pour but d'analyser, préparer les terrains dans le but 
+          de futures constructions.</p>
+          <p class="lead">Beaucoup de particuliers Réunionnais font appel à nos services avant de se lancer dans un project de constrution ou d'agricultures.</p>
+          <p class="lead">TopoRun est une entreprise importante pour l'île depuis les années 60. Nous avons de nombreux projets en cours pour le confort et le rayonnement de l'île.</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="0" height="0" xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice" focusable="false">
+        <img src="images/topographie-2.jpg" alt="">
+        </svg>
+      </div>
+    </div>
+    </br>
+    </br>
+    </br>
+    </br>
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Une entreprise investie.</h2>
+        <p class="lead"></p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="0" height="0" xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice" focusable="false">
+        <img width="600" height="400" src="images/exemple-plan-topo.jpg" >
+        </svg>
+
+      </div>
+    </div>
     
   
   </main>
